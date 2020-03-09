cf648fa3d05a993a5cb8af1dad8d55980017658a
OsosKP <ososkp@gmail.com>
2020-03-09 10:04:40 +0000

    Add script to handle fielding and catching

---
+++ module/fielding_pre.py
@@ -0,0 +1,34 @@
+import pandas as pd
+from retroid_dict import get_retroid
+pd.options.mode.chained_assignment = None  # default='warn'
+
+df = pd.read_csv(
+    'data/lahman/mlb_data/Fielding.csv').sort_values('playerID')
+
+df['playerID'] = df['playerID'].apply(get_retroid)
+df.rename(columns={'playerID': 'retroID'}, inplace=True)
+
+columns_to_drop = ['stint', 'teamID', 'lgID', 'G']
+df.drop(columns=columns_to_drop, inplace=True)
+df_catchers = df[df['POS'] == 'C']
+
+df_catchers['GS'].fillna(value=0, inplace=True)
+df_catchers['InnOuts'].fillna(value=0, inplace=True)
+df_catchers['WP'].fillna(value=0, inplace=True)
+df_catchers['SB'].fillna(value=0, inplace=True)
+df_catchers['CS'].fillna(value=0, inplace=True)
+df_catchers['ZR'].fillna(value=0, inplace=True)
+
+df['GS'].fillna(value=0, inplace=True)
+df['InnOuts'].fillna(value=0, inplace=True)
+catcher_columns = ['PB', 'WP', 'SB', 'CS', 'ZR']
+df.drop(columns=catcher_columns, inplace=True)
+df = df[df['POS'] != 'C']
+df.drop(columns=['yearID'], inplace=True)
+df_catchers.drop(columns=['yearID'], inplace=True)
+df['E'].fillna(value=0, inplace=True)
+df = df.groupby('retroID').sum().reset_index()
+df_catchers = df_catchers.groupby('retroID').sum().reset_index()
+
+df.to_csv('output/fielding.csv')
+df_catchers.to_csv('output/catching.csv')
